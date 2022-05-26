e051f08e530cf1eb01b8d6c640d307441df005e0
Tom <Tom>
2022-05-26 15:31:20 +0100

    Initial data access/context work

---
+++ src/SFA.DAS.ProviderEarnings/SFA.DAS.ProviderEarnings.DataAccess/ProviderEarningsDataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.ProviderEarnings.Domain.Entities;
+
+namespace SFA.DAS.ProviderEarnings.DataAccess
+{
+    public class ProviderEarningsDataContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ProviderEarningsDataContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public virtual DbSet<Earning> Earnings { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ProviderEarnings/SFA.DAS.ProviderEarnings.Database/Earning.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Earning]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
