48040f7dd9f0e5cdd4732266041b622b7bfce0b6
Tom <Tom>
2022-06-04 09:58:39 +0100

    data context migrated from old structure

---
+++ src/SFA.DAS.Funding.ApprenticeshipEarnings/SFA.DAS.Funding.ApprenticeshipEarnings.DataAccess/ApprenticeshipEarningsDataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Funding.ApprenticeshipEarnings.DataAccess.Entities;
+
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.DataAccess
+{
+    public class ApprenticeshipEarningsDataContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public ApprenticeshipEarningsDataContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public virtual DbSet<Earning> Earnings { get; set; }
+    }
+}
