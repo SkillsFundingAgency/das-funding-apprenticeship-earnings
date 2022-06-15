05749fc0e409ce94bb6086cc7045ddedcd70a058
Tom <Tom>
2022-06-15 16:30:28 +0100

    Infrastructure code

---
+++ src/SFA.DAS.Funding.ApprenticeshipEarnings/Infrastructure/Configuration/ApplicationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.Infrastructure.Configuration
+{
+    public class ApplicationSettings
+    {
+        public string AzureWebJobsStorage { get; set; }
+        public string ServiceBusConnectionString { get; set; }
+        public string QueueName { get; set; }
+        public string TopicPath { get; set; }
+    }
+}
