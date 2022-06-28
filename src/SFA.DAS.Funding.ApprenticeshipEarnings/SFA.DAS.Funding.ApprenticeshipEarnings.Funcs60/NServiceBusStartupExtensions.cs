7b26adf3617e7afadbfd164300783b4927b91ca3
ruslan <ruslanss@gmail.com>
2022-06-28 09:21:42 +0100

    add shared NServiceBus code

--- src/SFA.DAS.Funding.ApprenticeshipEarnings/SFA.DAS.Funding.ApprenticeshipEarnings.Funcs60/NServiceBusStartupExtensions.cs
+++ src/SFA.DAS.Funding.ApprenticeshipEarnings/SFA.DAS.Funding.ApprenticeshipEarnings.Funcs60/NServiceBusStartupExtensions.cs
@@ -1,55 +1,38 @@
-﻿//using Microsoft.Azure.WebJobs;
-//using Microsoft.Extensions.DependencyInjection;
-//using NServiceBus;
-//using NServiceBus.ObjectBuilder.MSDependencyInjection;
-////using SFA.DAS.NServiceBus.AzureFunction.Configuration;
-////using SFA.DAS.NServiceBus.AzureFunction.Hosting;
-////using SFA.DAS.NServiceBus.Configuration;
-////using SFA.DAS.NServiceBus.Configuration.AzureServiceBus;
-////using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
-////using SFA.DAS.NServiceBus.SqlServer.Configuration;
-//using SFA.DAS.UnitOfWork.NServiceBus.Configuration;
-//using System.Data.SqlClient;
-//using SFA.DAS.Funding.ApprenticeshipEarnings.Funcs60.das.shared;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.ObjectBuilder.MSDependencyInjection;
+using SFA.DAS.NServiceBus.AzureFunction.Configuration;
+using SFA.DAS.NServiceBus.AzureFunction.Hosting;
+using SFA.DAS.NServiceBus.Configuration;
+using SFA.DAS.NServiceBus.Configuration.AzureServiceBus;
+using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
 
-//namespace SFA.DAS.Funding.ApprenticeshipEarnings.Funcs60
-//{
-//    public static class NServiceBusStartupExtensions
-//    {
-//        public static IServiceCollection AddNServiceBus(
-//            this IServiceCollection serviceCollection,
-//            ServiceBusConfiguration configuration)
-//        {
-//            var webBuilder = serviceCollection.AddWebJobs(x => { });
-//            webBuilder.AddExecutionContextBinding();
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.Funcs60
+{
+    public static class NServiceBusStartupExtensions
+    {
+        public static IServiceCollection AddNServiceBus(
+            this IServiceCollection serviceCollection,
+            ServiceBusConfiguration configuration)
+        {
+            var webBuilder = serviceCollection.AddWebJobs(x => { });
+            webBuilder.AddExecutionContextBinding();
+            webBuilder.AddExtension(new NServiceBusExtensionConfigProvider(new NServiceBusOptions()));
 
-//            var endpointConfiguration = new EndpointConfiguration("sfa.das.whatever")
-//                .UseMessageConventions()
-//                .UseNewtonsoftJsonSerializer()
-//                .UseOutbox(true)
-//                .UseSqlServerPersistence(() => new SqlConnection(configuration.DbConnectionString))
-//                .UseUnitOfWork();
+            var endpointConfiguration = new EndpointConfiguration("sfa.das.whatever")
+                    .UseMessageConventions()
+                    .UseNewtonsoftJsonSerializer()
+                ;
 
-//            endpointConfiguration
-//                .UseAzureServiceBusTransport(configuration.NServiceBusConnectionString, r => r.AddRouting());
+            endpointConfiguration.UseAzureServiceBusTransport(configuration.NServiceBusConnectionString, r => r.AddRouting());
 
-//            var endpointWithExternallyManagedServiceProvider =
-//                EndpointWithExternallyManagedServiceProvider.Create(endpointConfiguration, serviceCollection);
-//            endpointWithExternallyManagedServiceProvider.Start(new UpdateableServiceProvider(serviceCollection));
-//            serviceCollection.AddSingleton(p => endpointWithExternallyManagedServiceProvider.MessageSession.Value);
+            var endpointWithExternallyManagedServiceProvider =
+                EndpointWithExternallyManagedServiceProvider.Create(endpointConfiguration, serviceCollection);
+            endpointWithExternallyManagedServiceProvider.Start(new UpdateableServiceProvider(serviceCollection));
+            serviceCollection.AddSingleton(p => endpointWithExternallyManagedServiceProvider.MessageSession.Value);
 
-//            return serviceCollection;
-//        }
-
-//        public static IServiceCollection AddNServiceBusMessageHandlers(
-//            this IServiceCollection serviceCollection,
-//            ServiceBusConfiguration configuration)
-//        {
-//            var webBuilder = serviceCollection.AddWebJobs(x => { });
-//            webBuilder.AddExecutionContextBinding();
-//            webBuilder.AddExtension(new NServiceBusExtensionConfigProvider(new NServiceBusOptions()));
-
-//            return serviceCollection;
-//        }
-//    }
-//}
+            return serviceCollection;
+        }
+    }
+}
---
+++ src/SFA.DAS.Funding.ApprenticeshipEarnings/SFA.DAS.Shared/SFA.DAS.NServiceBus.AzureFunction/Hosting/NServiceBusTriggerBindingProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Triggers;
+using SFA.DAS.NServiceBus.AzureFunction.Attributes;
+using SFA.DAS.NServiceBus.AzureFunction.Configuration;
+
+namespace SFA.DAS.NServiceBus.AzureFunction.Hosting
+{
+    public class NServiceBusTriggerBindingProvider : ITriggerBindingProvider
+    {
+        private readonly NServiceBusOptions _nServiceBusOptions;
+
+        public NServiceBusTriggerBindingProvider(NServiceBusOptions nServiceBusOptions = null)
+        {
+            _nServiceBusOptions = nServiceBusOptions ?? new NServiceBusOptions();
+        }
+
+        public Task<ITriggerBinding> TryCreateAsync(TriggerBindingProviderContext context)
+        {
+            var parameter = context.Parameter;
+            var attribute = parameter.GetCustomAttribute<NServiceBusTriggerAttribute>(false);
+
+            if (attribute == null)
+            {
+                return Task.FromResult<ITriggerBinding>(null);
+            }
+
+            if (string.IsNullOrEmpty(attribute.Connection))
+            {
+                attribute.Connection = EnvironmentVariables.NServiceBusConnectionString;
+            }
+
+            return Task.FromResult<ITriggerBinding>(new NServiceBusTriggerBinding(parameter, attribute, _nServiceBusOptions));
+        }
+    }
+}
