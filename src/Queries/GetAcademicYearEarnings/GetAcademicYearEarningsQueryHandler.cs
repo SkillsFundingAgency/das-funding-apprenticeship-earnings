011055ebc7df1960df60d38fe28ac8f7b96a4954
Michael Young <mick.young1981@googlemail.com>
2023-01-27 10:39:47 +0000

    Added co-investment amounts

--- src/Queries/GetAcademicYearEarnings/GetAcademicYearEarningsQueryHandler.cs
+++ src/Queries/GetAcademicYearEarnings/GetAcademicYearEarningsQueryHandler.cs
@@ -1,4 +1,7 @@
-﻿using SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Repositories;
+﻿using SFA.DAS.Apprenticeships.Types;
+using SFA.DAS.Funding.ApprenticeshipEarnings.DataTransferObjects;
+using SFA.DAS.Funding.ApprenticeshipEarnings.Domain;
+using SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Repositories;
 using SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Services;
 using SFA.DAS.Funding.ApprenticeshipEarnings.Infrastructure.Queries;
 
@@ -18,10 +21,27 @@ namespace SFA.DAS.Funding.ApprenticeshipEarnings.Queries.GetAcademicYearEarnings
         public async Task<GetAcademicYearEarningsResponse> Handle(GetAcademicYearEarningsRequest query, CancellationToken cancellationToken = default)
         {
             var academicYearEarnings = await _earningsQueryRepository.GetAcademicYearEarnings(query.Ukprn, _academicYearService.CurrentAcademicYear);
-            
+
+            academicYearEarnings.Learners.ForEach(l => CalculateCoInvestmentAmounts(l));
+
             var response = new GetAcademicYearEarningsResponse { AcademicYearEarnings = academicYearEarnings };
 
             return await Task.FromResult(response);
+        }
+
+        private void CalculateCoInvestmentAmounts(Learner learner)
+        {
+            if (learner.FundingType == FundingType.NonLevy)
+            {
+                learner.OnProgrammeEarnings.ForEach(e => CalculateCoInvestmentForEarning(e));
+            }
+        }
+
+        private void CalculateCoInvestmentForEarning(OnProgrammeEarning onProgrammeEarning)
+        {
+            var coinvestment = CoInvestment.Calculate(onProgrammeEarning.Amount);
+            onProgrammeEarning.GovernmentContribution = coinvestment.GovernmentContribution;
+            onProgrammeEarning.EmployerContribution = coinvestment.EmployerContribution;
         }
     }
 }
