e80023cc2ef5d956469d62d8a381889e4b3745f1
Michael Young <mick.young1981@googlemail.com>
2024-09-10 13:56:52 +0100

    Remove commented out code.

--- src/Domain/Repositories/ApprenticeshipRepository.cs
+++ src/Domain/Repositories/ApprenticeshipRepository.cs
@@ -44,28 +44,6 @@ public class ApprenticeshipRepository : IApprenticeshipRepository
 
     public async Task Update(Apprenticeship.Apprenticeship apprenticeship)
     {
-/*        var entity = apprenticeship.GetModel();
-        
-        foreach (var episode in entity.Episodes)
-        {
-            foreach (var price in episode.Prices)
-            {
-                //await DbContext.EpisodePrices.AddAsync(price);
-            }
-            await DbContext.EarningsProfiles.AddAsync(episode.EarningsProfile);
-            foreach (var instalment in episode.EarningsProfile.Instalments)
-            {
-                await DbContext.Instalments.AddAsync(instalment);
-            }
-            foreach (var earningsProfileHistory in episode.EarningsProfileHistory)
-            {
-                await DbContext.EarningsProfileHistories.AddAsync(earningsProfileHistory);
-                foreach (var instalment in earningsProfileHistory.Instalments)
-                {
-                    await DbContext.InstalmentHistories.AddAsync(instalment);
-                }
-            }
-        }*/
         await DbContext.SaveChangesAsync();
         await ReleaseEvents(apprenticeship);
     }
