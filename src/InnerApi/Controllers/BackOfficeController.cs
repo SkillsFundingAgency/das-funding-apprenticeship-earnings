38542e265366646d50b2f78ad5879722fcccf470
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-11-14 14:08:36 +0000

    FLP-999 ReRelease Earnings

---
+++ src/InnerApi/Controllers/BackOfficeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Funding.ApprenticeshipEarnings.Command;
+using SFA.DAS.Funding.ApprenticeshipEarnings.Command.ReReleaseEarningsGeneratedCommand;
+
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.InnerApi.Controllers;
+
+public class BackOfficeController : Controller
+{
+    ILogger<BackOfficeController> _logger;
+    ICommandDispatcher _commandDispatcher;
+
+    public BackOfficeController(ILogger<BackOfficeController> logger, ICommandDispatcher commandDispatcher)
+    {
+        _logger = logger;
+        _commandDispatcher = commandDispatcher;
+    }
+
+    [Route("ReReleaseEarningsGenerated")]
+    [HttpPost]
+    public async Task<IActionResult> ReReleaseEarningsGenerated(long ukprn)
+    {
+        _logger.LogInformation("ReReleaseEarningsGenerated for Ukprn: {ukprn}", ukprn);
+
+        try
+        {
+            await _commandDispatcher.Send(new ReReleaseEarningsGeneratedCommand(ukprn));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error handling ReReleaseEarningsGenerated for Ukprn: {ukprn}", ukprn);
+            return new StatusCodeResult(418);
+        }
+
+        return Ok();
+    }
+}
