47db29ff6d23716b85367533f7edc02302f26fda
tom-gough <tom.gough.tgsd@outlook.com>
2025-03-11 10:05:42 +0000

    FLP-1035 code review fix

--- src/AcceptanceTests/Extensions/DateTimeExtensions.cs
+++ src/AcceptanceTests/Extensions/DateTimeExtensions.cs
@@ -1,14 +1,13 @@
-﻿namespace SFA.DAS.Funding.ApprenticeshipEarnings.AcceptanceTests.Extensions
-{
-    public static class DateTimeExtensions
-    {
-        public static (short AcademicYear, byte Period) ToAcademicYearAndPeriod(this DateTime date)
-        {
-            var twoDigitCalendarYear = short.Parse(date.Year.ToString().Substring(2, 2));
+﻿namespace SFA.DAS.Funding.ApprenticeshipEarnings.AcceptanceTests.Extensions;
 
-            return date.Month < 8 
-                ? (short.Parse($"{twoDigitCalendarYear - 1}{twoDigitCalendarYear}"), (byte)(date.Month + 5)) 
-                : (short.Parse($"{twoDigitCalendarYear}{twoDigitCalendarYear + 1}"), (byte)(date.Month - 7));
-        }
+public static class DateTimeExtensions
+{
+    public static (short AcademicYear, byte Period) ToAcademicYearAndPeriod(this DateTime date)
+    {
+        var twoDigitCalendarYear = short.Parse(date.Year.ToString().Substring(2, 2));
+
+        return date.Month < 8 
+            ? (short.Parse($"{twoDigitCalendarYear - 1}{twoDigitCalendarYear}"), (byte)(date.Month + 5)) 
+            : (short.Parse($"{twoDigitCalendarYear}{twoDigitCalendarYear + 1}"), (byte)(date.Month - 7));
     }
 }
