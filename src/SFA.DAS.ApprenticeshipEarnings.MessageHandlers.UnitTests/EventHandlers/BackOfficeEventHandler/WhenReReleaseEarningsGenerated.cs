3b781a079c7cda05bc3d42fd6adadf823601b2e8
Chris Foster <chrisfoster186@googlemail.com>
2025-03-19 17:32:30 +0000

    Fixes unit tests

--- src/SFA.DAS.ApprenticeshipEarnings.MessageHandlers.UnitTests/EventHandlers/BackOfficeEventHandler/WhenReReleaseEarningsGenerated.cs
+++ src/SFA.DAS.ApprenticeshipEarnings.MessageHandlers.UnitTests/EventHandlers/BackOfficeEventHandler/WhenReReleaseEarningsGenerated.cs
@@ -24,7 +24,7 @@ public class WhenReReleaseEarningsGenerated
     {
         _loggerMock = new Mock<ILogger<Handlers.BackOfficeEventHandler>>();
         _commandDispatcherMock = new Mock<ICommandDispatcher>();
-        _eventHandler = new MessageHandlers.Handlers.BackOfficeEventHandler(_commandDispatcherMock.Object);
+        _eventHandler = new MessageHandlers.Handlers.BackOfficeEventHandler(_commandDispatcherMock.Object, _loggerMock.Object);
     }
 
     [Test]
@@ -35,7 +35,7 @@ public class WhenReReleaseEarningsGenerated
         var requestData = new HttpRequestMessage();
 
         // Act
-        var result = await _eventHandler.ReReleaseEarningsGenerated(requestData, ukprn, _loggerMock.Object);
+        var result = await _eventHandler.ReReleaseEarningsGenerated(requestData, ukprn);
 
         // Assert
         result.Should().BeOfType<OkObjectResult>();
@@ -50,7 +50,7 @@ public class WhenReReleaseEarningsGenerated
         var requestData = new HttpRequestMessage();
 
         // Act
-        var result = await _eventHandler.ReReleaseEarningsGenerated(requestData, ukprn, _loggerMock.Object);
+        var result = await _eventHandler.ReReleaseEarningsGenerated(requestData, ukprn);
 
         // Assert
         result.Should().BeOfType<BadRequestObjectResult>();
@@ -67,7 +67,7 @@ public class WhenReReleaseEarningsGenerated
             .ThrowsAsync(new Exception("Test exception"));
 
         // Act
-        var result = _eventHandler.ReReleaseEarningsGenerated(requestData, ukprn, _loggerMock.Object);
+        var result = _eventHandler.ReReleaseEarningsGenerated(requestData, ukprn);
         Action act = () => result.GetAwaiter().GetResult();
 
         // Assert
