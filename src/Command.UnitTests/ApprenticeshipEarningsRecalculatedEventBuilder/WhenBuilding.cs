4be0e3fd2c9526fce2ade646b7b6feab465c9274
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-05-15 11:39:12 +0100

    Fix fragile unit tests

--- src/Command.UnitTests/ApprenticeshipEarningsRecalculatedEventBuilder/WhenBuilding.cs
+++ src/Command.UnitTests/ApprenticeshipEarningsRecalculatedEventBuilder/WhenBuilding.cs
@@ -12,8 +12,6 @@ public class WhenBuilding
 {
     private Fixture _fixture;
     private Mock<ISystemClockService> _mockSystemClockService;
-    private Apprenticeship? _apprenticeship;
-    private ApprenticeshipModel? _apprenticeshipEntityModel;
     private Command.ApprenticeshipEarningsRecalculatedEventBuilder _builder;
 
     [SetUp]
@@ -28,42 +26,20 @@ public class WhenBuilding
     public void ThenTheApprenticeshipEarningsRecalculatedEventIsCorrectlyBuilt()
     {
         // Arrange
-        BuildApprenticeship();
+        var apprenticeship = _fixture.BuildApprenticeship();
         _mockSystemClockService.Setup(x => x.UtcNow).Returns(DateTimeOffset.UtcNow);
 
         // Act
-        var result = _builder.Build(_apprenticeship);
+        var result = _builder.Build(apprenticeship);
 
         // Assert
-        var currentEpisode = _apprenticeship.GetCurrentEpisode(_mockSystemClockService.Object);
+        var currentEpisode = apprenticeship.GetCurrentEpisode(_mockSystemClockService.Object);
 
-        result.ApprenticeshipKey.Should().Be(_apprenticeship.ApprenticeshipKey);
+        result.ApprenticeshipKey.Should().Be(apprenticeship.ApprenticeshipKey);
         result.DeliveryPeriods.Should().BeEquivalentTo(currentEpisode.BuildDeliveryPeriods());
         result.EarningsProfileId.Should().Be(currentEpisode.EarningsProfile!.EarningsProfileId);
         result.StartDate.Should().Be(currentEpisode.Prices!.OrderBy(x => x.StartDate).First().StartDate);
         result.PlannedEndDate.Should().Be(currentEpisode.Prices!.OrderBy(x => x.StartDate).Last().EndDate);
         result.AgeAtStartOfApprenticeship.Should().Be(currentEpisode.AgeAtStartOfApprenticeship);
-    }
-
-    private void BuildApprenticeship()
-    {
-        var episodeModel = _fixture
-            .Build<EpisodeModel>()
-            .With(x => x.Prices, new List<EpisodePriceModel>{ _fixture.Build<EpisodePriceModel>()
-                .With(x => x.StartDate, DateTime.UtcNow.AddMonths(-10))
-                .With(x => x.EndDate, DateTime.UtcNow.AddMonths(10))
-                .Create() })
-            .With(x => x.EarningsProfile, _fixture
-                .Build<EarningsProfileModel>()
-                .With(x => x.Instalments, new List<InstalmentModel>())
-                .With(x => x.AdditionalPayments, new List<AdditionalPaymentModel>())
-                .Create())
-            .Create();
-        _apprenticeshipEntityModel = _fixture
-            .Build<ApprenticeshipModel>()
-            .With(x => x.Episodes, new List<EpisodeModel>{ episodeModel })
-            .Create();
-        
-        _apprenticeship = Apprenticeship.Get(_apprenticeshipEntityModel);
     }
 }
