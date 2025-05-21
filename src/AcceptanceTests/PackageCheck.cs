20880f50ed1d2b4b48c98d3d27cb99c060678335
tom-gough <tom.gough.tgsd@outlook.com>
2025-05-21 09:52:14 +0100

    FLP-411 Moved package check logic to shared package

--- src/AcceptanceTests/PackageCheck.cs
+++ src/AcceptanceTests/PackageCheck.cs
@@ -1,58 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using SFA.DAS.Testing.PackageScanning;
 
-namespace SFA.DAS.Funding.ApprenticeshipEarnings.AcceptanceTests
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.AcceptanceTests;
+
+[TestFixture]
+public class PackageCheck
 {
-    [TestFixture]
-    public class PackageCheck
-    {
-        [Test]
-        //[Ignore("Ignored so that this test will not run on the build server, uncomment to use this check locally.")]
-        public void CheckForVulnerableAndDeprecatedPackages()
-        {
-            string projectDir = @"..\..\..\..\";
-
-            var csprojs = Directory.GetFiles(projectDir, "*.csproj", SearchOption.AllDirectories);
-
-            foreach (var csprojPath in csprojs)
-            {
-                Console.WriteLine($"Checking: {csprojPath}");
-
-                var result = RunCheck(csprojPath);
-
-                result.Should().NotBeNullOrWhiteSpace();
-            }
-        }
-
-        private string RunCheck(string projectFilePath)
-        {
-            var processStartInfo = new ProcessStartInfo
-            {
-                FileName = "dotnet",
-                Arguments = $"list \"{projectFilePath}\" package --source nuget.org --vulnerable",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            using var process = Process.Start(processStartInfo);
-            string output = process.StandardOutput.ReadToEnd();
-            string error = process.StandardError.ReadToEnd();
-
-            process.WaitForExit();
-
-            if (process.ExitCode != 0)
-            {
-                throw new Exception($"Command failed for {projectFilePath}:\n{error}");
-            }
-
-            return output;
-        }
-    }
+    [Test]
+    [Ignore("Ignored so that this test will not run on the build server, uncomment to use this check locally.")]
+    public void CheckForVulnerableAndDeprecatedPackages() =>
+        PackageChecker.AssertNoVulnerableOrDeprecatedPackages(@"..\..\..\..\");
 }
