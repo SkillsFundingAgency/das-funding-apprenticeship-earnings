0850bc7532a3de18956815c3303663004a8002ec
tom-gough <tom.gough.tgsd@outlook.com>
2025-05-22 10:49:39 +0100

    FLP-1060 Added new DB tables for Maths & English

---
+++ src/DataAccess/Entities/MathsAndEnglishModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SFA.DAS.Funding.ApprenticeshipEarnings.DataAccess.Entities;
+
+[Dapper.Contrib.Extensions.Table("Domain.MathsAndEnglish")]
+[Table("MathsAndEnglish", Schema = "Domain")]
+public class MathsAndEnglishModel
+{
+    public MathsAndEnglishModel() { }
+
+    public MathsAndEnglishModel(Guid episodeKey, string course, DateTime startDate, DateTime? endDate, decimal amount)
+    {
+        Key = Guid.NewGuid();
+        EpisodeKey = episodeKey;
+        Course = course;
+        StartDate = startDate;
+        EndDate = endDate;
+        Amount = amount;
+    }
+
+    [Dapper.Contrib.Extensions.Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public Guid Key { get; set; }
+
+    public Guid EpisodeKey { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public string Course { get; set; } = null!;
+
+    public decimal Amount { get; set; }
+
+    public List<MathsAndEnglishInstalmentModel> Instalments { get; set; } = [];
+}
---
+++ src/Database/Tables/MathsAndEnglish.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Domain].[MathsAndEnglish]
+(
+    [Key] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [EpisodeKey] UNIQUEIDENTIFIER NOT NULL,
+    [StartDate] DATETIME NOT NULL,
+    [EndDate] DATETIME NULL,
+    [Course] NCHAR(50) NOT NULL,
+    [Amount] DECIMAL(15,5) NOT NULL
+)
+GO
