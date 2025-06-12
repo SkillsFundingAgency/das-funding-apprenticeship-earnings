ff4c7e72cdd39e2359e535fb401c05015218c725
Chris Foster <chrisfoster186@googlemail.com>
2025-06-12 17:47:48 +0100

    Adds logging around query store record update

--- src/Domain/Apprenticeship/Events/EarningsRecalculatedHandler.cs
+++ src/Domain/Apprenticeship/Events/EarningsRecalculatedHandler.cs
@@ -1,18 +1,25 @@
-﻿using SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Repositories;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Repositories;
 
 namespace SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Apprenticeship.Events;
 
 public class EarningsRecalculatedHandler : IDomainEventHandler<EarningsRecalculatedEvent>
 {
     private readonly IEarningsQueryRepository _repository;
-    
-    public EarningsRecalculatedHandler(IEarningsQueryRepository repository)
+    private readonly ILogger<EarningsRecalculatedHandler> _logger;
+
+    public EarningsRecalculatedHandler(IEarningsQueryRepository repository, ILogger<EarningsRecalculatedHandler> logger)
     {
         _repository = repository;
+        _logger = logger;
     }
 
     public async Task Handle(EarningsRecalculatedEvent @event, CancellationToken cancellationToken = default(CancellationToken))
     {
+        var stopwatch = Stopwatch.StartNew();
+        _logger.LogInformation($"EarningsRecalculatedHandler replacing query store records");
         await _repository.Replace(@event.Apprenticeship);
+        _logger.LogInformation($"EarningsRecalculatedHandler done replacing query store records, elapsed {stopwatch.ElapsedMilliseconds}ms");
     }
 }
