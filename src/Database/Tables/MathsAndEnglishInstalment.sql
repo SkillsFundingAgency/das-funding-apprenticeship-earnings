d4793f185784f22a6306447e0f17dcf82120d1d8
Chris Foster <chrisfoster186@googlemail.com>
2025-09-03 16:19:35 +0100

    Restores deleted table and deletes the correct one

---
+++ src/Database/Tables/MathsAndEnglishInstalment.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Domain].[MathsAndEnglishInstalment]
+(
+	[Key] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+    [MathsAndEnglishKey] UNIQUEIDENTIFIER NOT NULL,
+    [AcademicYear] SMALLINT NOT NULL,
+    [DeliveryPeriod] TINYINT NOT NULL,
+    [Amount] DECIMAL(15,5) NOT NULL
+)
