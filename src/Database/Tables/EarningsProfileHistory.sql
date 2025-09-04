775d8b955a4d1690debecaf5c9b3b18a90a5f9a7
Chris Foster <chrisfoster186@googlemail.com>
2025-09-04 10:50:05 +0100

    Final table tweaks

--- src/Database/Tables/EarningsProfileHistory.sql
+++ src/Database/Tables/EarningsProfileHistory.sql
@@ -4,5 +4,15 @@
 	[EarningsProfileId] UNIQUEIDENTIFIER NOT NULL,
 	[Version] UNIQUEIDENTIFIER NOT NULL,
 	[CreatedOn] DATETIME NOT NULL DEFAULT GETDATE(),
-	[State] NVARCHAR(MAX) NOT NULL
+	[State] NVARCHAR(MAX) NOT NULL,
+	
+	CONSTRAINT FK_EarningsProfileHistory_EarningsProfile
+        FOREIGN KEY ([EarningsProfileId])
+        REFERENCES Domain.[EarningsProfile]([EarningsProfileId])
 )
+GO
+
+CREATE NONCLUSTERED INDEX IX_EarningsProfileHistory_Version
+    ON [History].[EarningsProfileHistory] ([Version]);
+
+
