e7eaf5e91f58e39192e0d23c575420279e9173d3
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-12-03 16:06:22 +0000

    Acceptance test added

--- src/AcceptanceTests/Model/PriceChangeModel.cs
+++ src/AcceptanceTests/Model/PriceChangeModel.cs
@@ -3,7 +3,7 @@
 public class PriceChangeModel
 {
     public DateTime? EffectiveFromDate { get; set; }
-    public DateTime? ChangeRequestDate{ get; set; }
+    public DateTime? EndDate{ get; set; }
     public decimal? NewTrainingPrice { get; set; }
     public decimal? NewAssessmentPrice { get; set; }
 }
