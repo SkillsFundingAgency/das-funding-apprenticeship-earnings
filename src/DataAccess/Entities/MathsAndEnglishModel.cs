a6bfc510af8e365d015a70df2c3fcdf47f8f7a8f
Chris Foster <chrisfoster186@googlemail.com>
2026-01-05 11:21:40 +0000

    Refactoring

--- src/DataAccess/Entities/MathsAndEnglishModel.cs
+++ src/DataAccess/Entities/MathsAndEnglishModel.cs
@@ -8,20 +8,6 @@ public class MathsAndEnglishModel
 {
     public MathsAndEnglishModel() { }
 
-    public MathsAndEnglishModel(Guid earningsProfileId, string course, string learnAimRef, DateTime startDate, DateTime endDate, decimal amount, DateTime? withdrawalDate, DateTime? actualEndDate, int? priorLearningAdjustmentPercentage)
-    {
-        Key = Guid.NewGuid();
-        EarningsProfileId = earningsProfileId;
-        Course = course;
-        LearnAimRef = learnAimRef;
-        StartDate = startDate;
-        EndDate = endDate;
-        Amount = amount;
-        WithdrawalDate = withdrawalDate;
-        ActualEndDate = actualEndDate;
-        PriorLearningAdjustmentPercentage = priorLearningAdjustmentPercentage;
-    }
-
     [Dapper.Contrib.Extensions.Key]
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public Guid Key { get; set; }
