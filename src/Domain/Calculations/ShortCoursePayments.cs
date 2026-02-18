09d41c1c73833f1eddf353b234577fb7055d2c0f
tom-gough <tom.gough@talentconsulting.uk>
2026-02-18 11:23:07 +0000

    FLP-1524 added constants for the percentages for duration and payment for short courses calculation

--- src/Domain/Calculations/ShortCoursePayments.cs
+++ src/Domain/Calculations/ShortCoursePayments.cs
@@ -6,18 +6,22 @@ namespace SFA.DAS.Funding.ApprenticeshipEarnings.Domain.Calculations;
 
 public static class ShortCoursePayments
 {
+    private const double FirstPaymentDurationPercentage = 0.3;
+    private const decimal FirstPaymentPortionPercentage = 0.3m;
+    private const decimal SecondPaymentPortionPercentage = 0.7m;
+
     public static List<Instalment> GenerateShortCoursePayments(decimal totalPrice, DateTime startDate, DateTime endDate, Guid priceKey, DateTime? completionDate)
     {
         var payments = new List<OnProgramPayment>();
 
         var duration = (endDate - startDate).Days + 1;
-        var firstPaymentDate = startDate.AddDays(Math.Floor(duration * 0.3) - 1);
+        var firstPaymentDate = startDate.AddDays(Math.Floor(duration * FirstPaymentDurationPercentage) - 1);
 
         payments.Add(new OnProgramPayment
         {
             AcademicYear = firstPaymentDate.ToAcademicYear(),
             DeliveryPeriod = firstPaymentDate.ToDeliveryPeriod(),
-            Amount = totalPrice * 0.3m,
+            Amount = totalPrice * FirstPaymentPortionPercentage,
             PriceKey = priceKey
         });
 
@@ -25,7 +29,7 @@ public static class ShortCoursePayments
         {
             AcademicYear = completionDate?.ToAcademicYear() ?? endDate.ToAcademicYear(),
             DeliveryPeriod = completionDate?.ToDeliveryPeriod() ?? endDate.ToDeliveryPeriod(),
-            Amount = totalPrice * 0.7m,
+            Amount = totalPrice * SecondPaymentPortionPercentage,
             PriceKey = priceKey
         });
 
