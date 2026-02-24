c357e387d0cf37e024ae4ee1c9330029e6be9c2b
tom-gough <tom.gough@talentconsulting.uk>
2026-02-24 11:58:20 +0000

    FLP-1493 added pre deployment script from FLP-1493

---
+++ src/Database/Scripts/FLP-1493-PreDeployment-DeleteAllData.sql
@@ -0,0 +1,21 @@
+-- Script to clear all data from the database
+
+BEGIN TRAN
+
+DELETE FROM [Domain].[MathsAndEnglishPeriodInLearning];
+DELETE FROM [Domain].[MathsAndEnglishInstalment];
+DELETE FROM [Domain].[MathsAndEnglish];
+
+DELETE FROM [Domain].[Instalment];
+DELETE FROM [Domain].[AdditionalPayment];
+DELETE FROM [History].[EarningsProfileHistory];
+DELETE FROM [Domain].[EarningsProfile];
+
+DELETE FROM [Domain].[EpisodePrice];
+DELETE FROM [Domain].[EpisodePeriodInLearning];
+DELETE FROM [Domain].[Episode];
+
+DELETE FROM [Domain].[Apprenticeship];
+
+COMMIT TRAN
+GO
